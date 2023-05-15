80dec80ab1e3510f995932878764994899480ae7
Timeward <69775225+Timeward@users.noreply.github.com>
2023-05-15 14:11:28 +0000

    Created method for showing div on select

--- Projeto/components/script.js
+++ Projeto/components/script.js
@@ -1,22 +1,7 @@
-const dropdown = document.getElementById('dropdown');
-const resizeDiv = document.getElementById('resize');
-const hexDiv = document.getElementById('hex');
-const ovalDiv = document.getElementById('oval');
-
-dropdown.addEventListener('change', function() {
-    if(dropdown.value === 'resize'){
-        resizeDiv.style.display = 'flex';
-        hexDiv.style.display = 'none';
-        ovalDiv.style.display = 'none';
-    } 
-    else if(dropdown.value === 'hex'){
-        resizeDiv.style.display = 'none';
-        hexDiv.style.display = 'flex';
-        ovalDiv.style.display = 'none';
-    } 
-    else if(dropdown.value === 'oval'){
-        resizeDiv.style.display = 'none';
-        hexDiv.style.display = 'none';
-        ovalDiv.style.display = 'flex';
+function show(selectValue){
+    var showResizeDiv = document.getElementById("resizeDiv");
+    showResizeDiv.classList.remove("showDiv");
+    if(selectValue === "resize"){
+        showResizeDiv.classList.add("showDiv");
     }
-});
+}
