b3324f90f72752d9f0e680e8faa219c396cd3549
Timeward <sonicx1932@gmail.com>
2023-05-15 13:23:41 -0300

    HTML AND CSS COMPLETE

--- Projeto/components/script.js
+++ Projeto/components/script.js
@@ -1,7 +1,21 @@
 function show(selectValue){
     var showResizeDiv = document.getElementById("resizeDiv");
+    var showHexDiv = document.getElementById("hexDiv");
+    var showOvalDiv = document.getElementById("ovalDiv");
+    
     showResizeDiv.classList.remove("showDiv");
+    showHexDiv.classList.remove("showDiv");
+    showOvalDiv.classList.remove("showDiv");
+
     if(selectValue === "resize"){
         showResizeDiv.classList.add("showDiv");
+    }
+
+    if(selectValue === "hex"){
+        showHexDiv.classList.add("showDiv");
+    }
+
+    if(selectValue === "oval"){
+        showOvalDiv.classList.add("showDiv");
     }
 }
